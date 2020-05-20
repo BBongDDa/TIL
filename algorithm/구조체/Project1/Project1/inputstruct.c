3c6be7df5e15c282eba38ae22fd1adec51ca1615
BBongDDa <shsiskd@gmail.com>
2020-05-20 23:55:42 +0900

    구조체 추가

---
+++ algorithm/구조체/Project1/Project1/inputstruct.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+	char name[12];
+	int age;
+	double height;
+};
+
+int main() {
+	struct student s_list[10];
+	for(i)
+
+
+	for (int i = 0; i <= 6; i++) {
+		printf("이름 = %s, 나이 = %d, 키 = %6.2f\n", s_list[i].name, s_list[i].age, s_list[i].height);
+	}
+	return 0;
+}
+
