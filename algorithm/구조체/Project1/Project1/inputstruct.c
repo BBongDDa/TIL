44c53ba97be1eef87307aa06b624ee4e12b08b92
BBongDDa <shsiskd@gmail.com>
2020-05-21 00:47:15 +0900

    구조체에 scanf 추가

--- algorithm/구조체/Project1/Project1/inputstruct.c
+++ algorithm/구조체/Project1/Project1/inputstruct.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 struct student {
-	char name[12];
+	char name[30];
 	int age;
-	double height;
-};
+	float height;
+}s_list;
 
 int main() {
-	struct student s_list[10];
-	for(i)
+	printf("학생 건강정보 관리 시스템\n");
+	struct student s_list[5] = { 0 };
+    int i=0, result=0;
+    for (i = 0; i < 5; i++)//5번 반복하는 for문
+    {
+        printf("이름 입력 : \n"); scanf_s("%s", s_list[i].name,30); //이름 입력 받아 저장
+        printf("나이 입력 : \n"); scanf_s("%d", &s_list[i].age); //나이를 입력 받아 저장
+        printf("키 입력 : \n"); scanf_s("%f", &s_list[i].height);
+    }
 
-
-	for (int i = 0; i <= 6; i++) {
-		printf("이름 = %s, 나이 = %d, 키 = %6.2f\n", s_list[i].name, s_list[i].age, s_list[i].height);
+	for (result = 0; result < 5; result++) {
+		printf("---- %d번째 학생 정보 입력  ---- \n", result + 1);
+		printf("이름 = %s, 나이 = %d, 키 = %6.2f\n", s_list[result].name, s_list[result].age, s_list[result].height);
 	}
 	return 0;
 }
--- algorithm/구조체/Project1/Project1/struct.c
+++ algorithm/구조체/Project1/Project1/struct.c
@@ -1,25 +1,25 @@
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//struct student {
-//	char name[12];
-//	int age;
-//	double height;
-//};
-//
-// void main() {
-//		struct student S_list[] = {
-//			{ "문경수", 46, 168.5 },
-//			{ "노태우", 45, },
-//			{ "문정", 7, 153.4 },
-//			{ "권정", 58, 162.0 },
-//			{ "최상균", 41, 185.0 },
-//			{ "김동승", },
-//			{ "이기남", 49, 182.0 },
-//		};
-//	for (int i = 0; i <= 6; i++) {
-//		printf("이름 = %s, 나이 = %d, 키 = %6.2f\n", S_list[i].name, S_list[i].age, S_list[i].height);
-//	}
-//	system("pause");
-//}
-//
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+	char name[12];
+	int age;
+	double height;
+};
+
+ void main() {
+		struct student s_list[] = {
+			{ "문경수", 46, 168.5 },
+			{ "노태우", 45, },
+			{ "문정", 7, 153.4 },
+			{ "권정", 58, 162.0 },
+			{ "최상균", 41, 185.0 },
+			{ "김동승", },
+			{ "이기남", 49, 182.0 },
+		};
+	for (int i = 0; i <= 6; i++) {
+		printf("이름 = %s, 나이 = %d, 키 = %6.2f\n", s_list[i].name, s_list[i].age, s_list[i].height);
+	}
+	system("pause");
+}
+
